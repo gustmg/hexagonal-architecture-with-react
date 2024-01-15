9d7bcf5417445ded8f888b7b8576eb0c00de7275
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-15 16:52:46 -0600

    feat: :lipstick: Se importa tipografía Roboto en punto de entrada de aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const router = createBrowserRouter([
   {
