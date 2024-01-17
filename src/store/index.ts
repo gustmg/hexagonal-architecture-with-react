0455802b89733db350558e955745b35e0a529dc5
Gustavo Mitre <gustavo.mitre.gallardo@gmail.com>
2024-01-16 19:14:24 -0600

    feat(infrastructure): :sparkles: Se integra slice de vehículos a store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./customerSlice";
 import snackbarSlice from "./snackbarSlice";
+import vehicleSlice from "./vehicleSlice";
 
 export const store = configureStore({
   reducer: {
     customer: customerReducer,
     snackbar: snackbarSlice,
+    vehicle: vehicleSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
